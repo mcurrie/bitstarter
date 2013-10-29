c59cbfe799d9a0df5cc5ceec0bee98928a559f4d
Michael James Currie <mcurrie_mjc@yahoo.com>
2013-10-29 10:05:54 +0000

    file read

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fs = require('fs');
+  //response.send('Hello World2!');
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
