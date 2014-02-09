6cedcc24995560952bec027fdb9a89493895f66c
Michael James Currie <mcurrie_mjc@yahoo.com>
2014-02-09 21:03:33 +0000

    doneX

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+
+//var app = express.createServer(express.logger());
 var app = express();
-app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
   //response.send('Hello World2!');
-  response.send(fs.readFileSync('index.html').toString());
+  response.send(fs.readFileSync(htmlfile).toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
